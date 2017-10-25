2eede5a8d79704195f64f891e6dcab4e08c15c48
Ozzie Gooen <Ozzie@grandroundshealth.com>
2017-10-24 18:25:58 -0700

    Some progress to create payoutTokenGifts

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -9,6 +9,10 @@ const Query = {
       where: { id: data.id },
     });
   },
+  allTokenGifts: () => {
+    return db.TokenGift.findAll({})
+    .then(e => e.map(e => e.dataValues))
+  },
   TokenGift: (_, data) => {
     return db.TokenGift.findOne({
       where: { id: data.id }
@@ -33,6 +37,20 @@ const Mutation = {
   },
   createUser: (_, data) => {
     return createUser(data)
+  },
+  payoutTokenGift:(_, data) => {
+    let item;
+    return db.TokenGift.findOne({
+      where: { id: data.id }
+    })
+    .then((tokenGift) => {
+      item = tokenGift;
+      return sendTokens({address: "0xdf7850D6CC6AFF77E8AFC42Accdc34B82099CaCC", amount: 1})
+    })
+    .then((transaction) => {
+      item.update({transactionHash: transaction.id})
+      .then(t => {return item.dataValues})
+    })
   },
   createTokenGift: () => {
     return db.TokenGift.create({})
